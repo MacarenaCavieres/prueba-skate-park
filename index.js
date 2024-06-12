7b6936b3fb2c2678f10dfd059b1e00f3467198e7
MCavieres <maca.c.91@gmail.com>
2024-06-11 21:06:17 -0400

    configuracion de hbs y ruta general para render hbs

---
+++ index.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import express from "express";
+import path from "path";
+import viewsRoute from "./routes/views.route.js";
+import { engine } from "express-handlebars";
+
+const app = express();
+
+const __dirname = import.meta.dirname;
+
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname + "/views"));
+
+app.use(express.static("public"));
+app.use("/", viewsRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
