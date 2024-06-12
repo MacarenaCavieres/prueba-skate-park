054df1bf619fe4acab84046cd942c8b66dd3805e
MCavieres <maca.c.91@gmail.com>
2024-06-11 21:18:09 -0400

    se realiza views de admin.hbs y datos.hbs

--- routes/views.route.js
+++ routes/views.route.js
@@ -5,5 +5,17 @@ const router = Router();
 router.get("/", (req, res) => {
     res.render("home");
 });
+router.get("/login", (req, res) => {
+    res.render("login");
+});
+router.get("/registro", (req, res) => {
+    res.render("registro");
+});
+router.get("/datos", (req, res) => {
+    res.render("datos");
+});
+router.get("/admin", (req, res) => {
+    res.render("admin");
+});
 
 export default router;
